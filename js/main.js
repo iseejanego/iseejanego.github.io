5a2502dfc1b99466c853ad4755429ec2750d6be5
iseejanego <jane.kat.go@gmail.com>
2017-10-22 19:41:55 -0700

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var feed = new Instafeed({
+	get: 'user',
+	userId: 3551467404,
+	accessToken: '3551467404.ee0f22a.bad080471991479badfef4f9dd4ddeef'
+	//clientId: 'ee0f22a39f204688b46f01f6ab87523e'
+});
+
+feed.run();
